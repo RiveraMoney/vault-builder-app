2b0df6162c5f1ec5c95fe5833b8e674b4ecd80cd
HashHaran <hariharan@alumni.iitm.ac.in>
2022-12-27 06:41:38 +0530

    Replacing factory contract address with main net deployed factory contract address

--- src/app/service/global.service.ts
+++ src/app/service/global.service.ts
@@ -8,5 +8,5 @@ export class GlobalService {
   constructor() { }
 
   public masterChefAddress: any = '0xa5f8C5Dbd5F286960b9d90548680aE5ebFf07652';
-  public deployedContract: any = '0x44b3532E500Fa10Db045801e143F43182C23A7cF';
+  public deployedContract: any = '0xa1F54f9115c12Bd25f0dD81CC661Af8bD830D519';    //Main net factory contract deployment address
 }
