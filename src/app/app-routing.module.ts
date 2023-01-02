74f411c4c64ebe3e19832cb831cb80944a7473b5
sahil <sahil@authlink.com>
2023-01-02 18:05:47 +0530

    use hash in routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './component/home/home.component';
 import { ValutListComponent } from './component/valut-list/valut-list.component';
 import { VaultDetailsComponent } from './component/vault-details/vault-details.component';
@@ -14,7 +14,12 @@ const routes: Routes = [{
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,
+    {
+      useHash: true,
+      preloadingStrategy: PreloadAllModules
+    }
+  )],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
